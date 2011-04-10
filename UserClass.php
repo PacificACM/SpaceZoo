4538b7163bcaca00156284c68498d89d708a0ca7
Theino <theino8@yahoo.com>
2011-04-10 15:45:37 -0700

    added initial db Connection using SecretsClass

--- UserClass.php
+++ UserClass.php
@@ -1,8 +1,15 @@
 <?php
+require_once 'SecretClass.php'
 class UserClass
 {
     function __construct($id)
     {
+        $dbhost = 'localhost';
+        $dbuser = SecretsClass::$dbUser;
+        $dbpass = SecretsClass::$dbPassword;
+        $dbname = 'spacezoo_main'
+        $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+        mysql_select_db($dbname);
         //if user doesn't exist in db, then create
     }
 }
