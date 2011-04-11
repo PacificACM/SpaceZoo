d371cd1e47dd7a3df0ba9136a759c4c2d642a9a3
Theino <theino8@yahoo.com>
2011-04-11 01:26:41 -0700

    Fixed INSERT INTO sql statement problem. Decided to switch all sql queries to be surrounded with double quotes rather than single quotes

--- UserClass.php
+++ UserClass.php
@@ -10,16 +10,16 @@ class UserClass
         $dbname = 'spacezoo_main';
         $conn = mysql_connect($dbhost, $dbuser, $dbpass);
         mysql_select_db($dbname);
-        $result = mysql_query('SELECT user_id FROM user WHERE user_id = ' . $id);
+        $result = mysql_query("SELECT user_id FROM user WHERE user_id = $id");
         $foundUser = mysql_numrows($result);
         $currentDateTime = date("Y-m-d H:i:s");
         if($foundUser == 0)
         {
-            mysql_query('INSERT INTO user (user_id, firstSeen, lastSeen) VALUES ($id, $currentDateTime, $currentDateTime)');
+            mysql_query("INSERT INTO user (user_id, firstSeen, lastSeen) VALUES ('$id', '$currentDateTime', '$currentDateTime')");
         }
         else
         {
-            mysql_query('UPDATE user SET lastSeen = ' . $currentDateTime . ' WHERE user_id = ' . $id);
+            mysql_query("UPDATE user SET lastSeen = $currentDateTime WHERE user_id = $id");
         }
         mysql_close();
         $this->user_id = $id;
