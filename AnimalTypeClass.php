37af92e313d3cf3d62ad98201f108dfda4aa0f83
Theino <theino8@yahoo.com>
2011-04-12 01:23:19 -0700

    added getID, getName and getRarity methods to AnimalTypeClass

--- AnimalTypeClass.php
+++ AnimalTypeClass.php
@@ -11,5 +11,22 @@ class AnimalTypeClass
         mysql_query("INSERT INTO animals (name, type, rarity) VALUES ('Unnamed', $this->id, 1)");
         //need to fix rarity based on animalType own inherent rarity.
     }
+    function getID()
+    {
+        return $this->id;
+    }
+    function getName()
+    {
+        $result = mysql_query("SELECT name FROM animalTypes WHERE id = $this->id");
+        $row = mysql_fetch_assoc($result);
+        return $row['name'];
+    }
+    function getRarity()
+    {
+        $result = mysql_query("SELECT rarity FROM animalTypes WHERE id = $this->id");
+        $row = mysql_fetch_assoc($result);
+        return $row['rarity'];
+    }
+    
 }
 ?>
