a67f234ec73d61fc9553ccccba1a9bc4f9705a5d
Theino <theino8@yahoo.com>
2011-04-12 01:08:26 -0700

    added incomplete createNewAnimalOfType method

--- AnimalTypeClass.php
+++ AnimalTypeClass.php
@@ -6,6 +6,10 @@ class AnimalTypeClass
     {
         $this->id = $id;
     }
-    
+    function createNewAnimalOfType()
+    {
+        mysql_query("INSERT INTO animals (name, type, rarity) VALUES ('Unnamed', $this->id, 1)");
+        //need to fix rarity based on animalType own inherent rarity.
+    }
 }
 ?>
