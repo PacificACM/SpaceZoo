f6023048d43866ca2e15a7b3fe0a9e2c3679c320
Theino <theino8@yahoo.com>
2011-04-12 01:24:04 -0700

    changed names of methods from format getAnimalName to just getName

--- AnimalClass.php
+++ AnimalClass.php
@@ -16,19 +16,19 @@ class AnimalClass
     {
         mysql_query("UPDATE animals SET rarity = $rarity WHERE user_id = $this->id");
     }
-    function getAnimalTypeID()
+    function getTypeID()
     {
         $result = mysql_query("SELECT type FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
         return $row['type'];
     }
-    function getAnimalName()
+    function getName()
     {
         $result = mysql_query("SELECT name FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
         return $row['name'];
     }
-    function setAnimalName($name)
+    function setName($name)
     {
         mysql_query("UPDATE animals SET name = $name WHERE user_id = $this->id");
     }
