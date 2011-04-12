8e198a2cfd9aa4d4e93c628a8cfafbd746c050f0
Theino <theino8@yahoo.com>
2011-04-12 00:49:33 -0700

    changed animal_id to id for consistency

--- AnimalClass.php
+++ AnimalClass.php
@@ -4,33 +4,33 @@ class AnimalClass
 {
     function __construct($id)
     {
-        $this->animal_id = $id;
+        $this->id = $id;
     }
     function getRarity()
     {
-        $result = mysql_query("SELECT rarity FROM animals WHERE id = $this->animal_id");
+        $result = mysql_query("SELECT rarity FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
         return $row['rarity'];
     }
     function setRarity($rarity)
     {
-        mysql_query("UPDATE animals SET rarity = $rarity WHERE user_id = $this->animal_id");
+        mysql_query("UPDATE animals SET rarity = $rarity WHERE user_id = $this->id");
     }
     function getAnimalTypeID()
     {
-        $result = mysql_query("SELECT type FROM animals WHERE id = $this->animal_id");
+        $result = mysql_query("SELECT type FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
         return $row['type'];
     }
     function getAnimalName()
     {
-        $result = mysql_query("SELECT name FROM animals WHERE id = $this->animal_id");
+        $result = mysql_query("SELECT name FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
         return $row['name'];
     }
     function setAnimalName($name)
     {
-        mysql_query("UPDATE animals SET name = $name WHERE user_id = $this->animal_id");
+        mysql_query("UPDATE animals SET name = $name WHERE user_id = $this->id");
     }
 }
 ?>
