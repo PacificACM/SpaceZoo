1f4af475eedd1e7e7960acd5fe2f614fafe2c6f4
Theino <theino8@yahoo.com>
2011-04-13 20:07:14 -0700

    Added MenuItemClass.php and added addMenuItem to MenuClass.php

--- MenuClass.php
+++ MenuClass.php
@@ -1,9 +1,15 @@
 <?php
+require_once 'MenuItemClass.php';
 class Menu
 {
+    private $menuItems;
     function __construct()
     {
+        $this->menuItems = array();
     }
-    
+    function addMenuItem($newMenuItem)
+    {
+        $this->menuItems[] = $newMenuItem;
+    }
 }
 ?>
---
+++ MenuItemClass.php
@@ -0,0 +1,12 @@
+<?php
+class MenuItem
+{
+    private $name;
+    private $path;
+    function __construct($Name, $Path)
+    {
+        $name = $Name;
+        $path = $Path;
+    }
+}
+?>
