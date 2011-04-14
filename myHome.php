0a7bedc5e68aa88c4ef595ba6f7f18f2a70a1c26
Theino <theino8@yahoo.com>
2011-04-14 11:25:58 -0700

    added a simple money table and got the connection with facebook working

--- myHome.php
+++ myHome.php
@@ -1,4 +1,30 @@
 <!doctype html>
+<?php
+
+    require 'facebook-php-sdk/src/facebook.php';
+    function __autoload($className) {
+      include $className . '.php';
+    }
+    // Create our Application instance (replace this with your appId and secret).
+    $facebook = new Facebook(array(
+      'appId'  => '184154878290481',
+      'secret' => SecretsClass::$secret,
+      'cookie' => true,
+    ));
+    $session = $facebook->getSession();
+
+    $me = null;
+    // Session based API call.
+    if ($session) {
+      try {
+        $uid = $facebook->getUser();
+        $me = $facebook->api('/me');
+      } catch (FacebookApiException $e) {
+        error_log($e);
+      }
+    }
+    
+?>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
     <head>
     <title>SpaceZoo</title>
@@ -6,5 +32,12 @@
   </head>
   <body>
     This is your home zoo! Go back to <a href='index.php'>index</a>
+    <table>
+        <?php
+            $user = new UserClass($facebook->getUser());
+            $money = $user->getMoney();
+            echo "<tr><td>Money</td><td>$money</td></tr>";
+        ?>
+    </table>
   </body>
 </html>
