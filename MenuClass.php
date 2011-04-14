e42769120ea156975d41130f8713edc1a5469cd6
Theino <theino8@yahoo.com>
2011-04-13 22:29:19 -0700

    moved the CSS stuff to default.css

--- MenuClass.php
+++ MenuClass.php
@@ -14,15 +14,14 @@ class Menu
     
     function printMenu()
     {
-        echo '<div style="width: 100%; background: #fff; float: left;">';
+        echo '<div id="menu">';
         
         foreach($this->menuItems as $menuItem)
         {
             
-            echo '<ul style="list-style: none; margin: 0; padding: 0; width: 12em; float: left;">';
-            echo '<h2 style="font: bold 11px/16px arial, helvetica, sans-serif; display: block; border-width: 1px; border-style: solid; border-color: #ccc #888 #555 #bbb; margin: 0; padding: 2px 3px;">';
+            echo '<ul>';
             $menuItem->printMenuItem();
-            echo '</h2></ul>';
+            echo '</ul>';
         }
         echo '</div>';
     }
