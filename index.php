0b1c8eff04801def378bd1b55081454846ef228b
Theino <theino8@yahoo.com>
2011-04-13 21:27:26 -0700

    added some css to make links "prettier"

--- index.php
+++ index.php
@@ -63,19 +63,8 @@ $naitik = $facebook->api('/naitik');
 <!doctype html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
-    <title>php-sdk</title>
-    <style>
-      body {
-        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
-      }
-      h1 a {
-        text-decoration: none;
-        color: #3b5998;
-      }
-      h1 a:hover {
-        text-decoration: underline;
-      }
-    </style>
+    <title>SpaceZoo</title>
+    <link rel="stylesheet" type="text/css" href="default.css" /> 
   </head>
   <body>
     <!--
