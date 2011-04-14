57ba65f37b51aa26ac89a04cab840dc90a7fa9b7
Theino <theino8@yahoo.com>
2011-04-13 23:49:39 -0700

    added basic myHome.php

---
+++ myHome.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+    <head>
+    <title>SpaceZoo</title>
+    <link rel="stylesheet" type="text/css" href="default.css" /> 
+  </head>
+  <body>
+    This is your home zoo! Go back to <a href='index.php'>index</a>
+  </body>
+</html>
