29f700171e0ceea657eae062d29e1da8e7787065
Theino <theino8@yahoo.com>
2011-04-15 15:40:25 -0700

    added a menu to myHome.php and the same header as in index

--- myHome.php
+++ myHome.php
@@ -9,9 +9,13 @@
     <link rel="stylesheet" type="text/css" href="default.css" /> 
   </head>
   <body>
-    This is your home zoo! Go back to <a href='index.php'>index</a>
+    <h1 style = "text-align: center;">Space Zoo</h1>
     <table>
         <?php
+            $menu = new MenuClass();
+            $menu->addMenuItem(new MenuItemClass('Index', 'index.php', false));
+            $menu->addMenuItem(new MenuItemClass('View Other Zoos', 'otherZoos.php', false));
+            $menu->printMenu();
             $user = new UserClass($facebook->getUser());
             $money = $user->getMoney();
             echo "<tr><td>Money</td><td>$money</td></tr>";
