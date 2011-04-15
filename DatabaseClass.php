45e8d718944d4dc2fd0fe7b7ddd77c807d537080
Theino <theino8@yahoo.com>
2011-04-15 14:36:13 -0700

    got rid of all methods of DatabaseClass. If I want to make a test double I can just make a new DatabaseClass and use a different databases connection info.

--- DatabaseClass.php
+++ DatabaseClass.php
@@ -10,21 +10,5 @@ class DatabaseClass
         $conn = mysql_connect($dbhost, $dbuser, $dbpass);
         mysql_select_db($dbname);
     }
-    function mysql_query($string)
-    {
-        return mysql_query($string);
-    }
-    function mysql_numrows($string)
-    {
-        return mysql_numrows($string);
-    }
-    function mysql_close()
-    {
-        return mysql_close();
-    }
-    function mysql_fetch_assoc($string)
-    {
-        return mysql_fetch_assoc($string);
-    }
 }
 ?>
