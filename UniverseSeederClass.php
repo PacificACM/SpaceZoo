4d60bd193db3829e8ee2a731d18200577abc4bb7
Theino <theino8@yahoo.com>
2011-04-16 23:35:21 -0700

    added basic UniverseSeederClass.php with start of addPlanetsToArea method

---
+++ UniverseSeederClass.php
@@ -0,0 +1,17 @@
+<?php
+class UniverseSeederClass
+{
+    function __construct()
+    {
+        
+    }
+    function addPlanetsToArea($xLocation1, $yLocation1, $xLocation2, $yLocation2, $planets)
+    {
+        
+        for($i = 0; $i < $planets; $i++)
+        {
+            
+        }
+    }
+}
+?>
