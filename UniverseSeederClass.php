355bdbd573471606d6bf0216203c3a556d3d2a7e
Theino <theino8@yahoo.com>
2011-04-17 12:42:27 -0700

    untested but mostly complete version of addPlanetsToArea method

--- UniverseSeederClass.php
+++ UniverseSeederClass.php
@@ -12,17 +12,23 @@ class UniverseSeederClass
         {
             $xRand = mt_rand($xLocation1,$xLocation2);
             $yRand = mt_rand($yLocation1,$yLocation2);
-            $xRandDecimal = mt_rand(0,100);
-            $yRandDecimal = mt_rand(0,100);
-            $xRand *= 100;
-            $yRand *= 100;
+            $xRandDecimal = mt_rand(0,1000);
+            $yRandDecimal = mt_rand(0,1000);
+            $xRand *= 1000;
+            $yRand *= 1000;
             $xRand += $xRandDecimal;
             $yRand += $yRandDecimal;
             $tempLow = mt_rand(-265,1000);
             $tempHighOffset = mt_rand(50,1000);
             $tempHigh = $tempLow + $tempHighOffset;
-            mysql_query("INSERT INTO planets (name, tempLow, tempHigh, xLocation, yLocation) VALUES ('Unnamed', $tempLow, $tempHigh, $xRand, $yRand))");
+            $result = mysql_query("SELECT id FROM planets WHERE (ABS(xLocation - $xRand) < 50) AND (ABS(yLocation - $yRand) < 50)");
+            $numRows = mysql_numrows($result);
+            if($numRows == 0)
+            {
+                mysql_query("INSERT INTO planets (name, tempLow, tempHigh, xLocation, yLocation) VALUES ('Unnamed', $tempLow, $tempHigh, $xRand, $yRand))");
+            }
         }
+        mysql_close();
     }
 }
 ?>
