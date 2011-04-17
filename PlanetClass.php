aab7ff1a2a53266f58f928cdaa1b4c4f34de0f4f
Theino <theino8@yahoo.com>
2011-04-16 22:49:21 -0700

    added getName method

--- PlanetClass.php
+++ PlanetClass.php
@@ -6,5 +6,12 @@ class PlanetClass
     {
         $this->id = $id;
     }
+    function getName()
+    {
+        $db = new DatabaseClass();
+        $result = mysql_query("SELECT name FROM planets WHERE id = $this->id");
+        $row = mysql_fetch_assoc($result);
+        return $row['name'];
+    }
 }
 ?>
