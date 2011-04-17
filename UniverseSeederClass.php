f39490ff79c75aa0804d461f858990c39732e1c9
Theino <theino8@yahoo.com>
2011-04-17 12:24:04 -0700

    added more the the still incomplete addPlanetsToArea method

--- UniverseSeederClass.php
+++ UniverseSeederClass.php
@@ -7,10 +7,21 @@ class UniverseSeederClass
     }
     function addPlanetsToArea($xLocation1, $yLocation1, $xLocation2, $yLocation2, $planets)
     {
-        
+        $db = new DatabaseClass();
         for($i = 0; $i < $planets; $i++)
         {
-            
+            $xRand = mt_rand($xLocation1,$xLocation2);
+            $yRand = mt_rand($yLocation1,$yLocation2);
+            $xRandDecimal = mt_rand(0,100);
+            $yRandDecimal = mt_rand(0,100);
+            $xRand *= 100;
+            $yRand *= 100;
+            $xRand += $xRandDecimal;
+            $yRand += $yRandDecimal;
+            $tempLow = mt_rand(-265,1000);
+            $tempHighOffset = mt_rand(50,1000);
+            $tempHigh = $tempLow + $tempHighOffset;
+            mysql_query("INSERT INTO planets (name, tempLow, tempHigh, xLocation, yLocation) VALUES ('Unnamed', $tempLow, $tempHigh, $xRand, $yRand))");
         }
     }
 }
