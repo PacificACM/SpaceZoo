b6e8d63d0edddadb3c0049528e2524d7e0651e3c
Theino <theino8@yahoo.com>
2011-04-17 21:47:25 -0700

    added appropriate mysql_close to methods

--- AnimalClass.php
+++ AnimalClass.php
@@ -11,18 +11,21 @@ class AnimalClass
         $db = new DatabaseClass();
         $result = mysql_query("SELECT rarity FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
+        mysql_close();
         return $row['rarity'];
     }
     function setRarity($rarity)
     {
         $db = new DatabaseClass();
         mysql_query("UPDATE animals SET rarity = $rarity WHERE user_id = $this->id");
+        mysql_close();
     }
     function getTypeID()
     {
         $db = new DatabaseClass();
         $result = mysql_query("SELECT type FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
+        mysql_close();
         return $row['type'];
     }
     function getName()
@@ -30,12 +33,14 @@ class AnimalClass
         $db = new DatabaseClass();
         $result = mysql_query("SELECT name FROM animals WHERE id = $this->id");
         $row = mysql_fetch_assoc($result);
+        mysql_close();
         return $row['name'];
     }
     function setName($name)
     {
         $db = new DatabaseClass();
         mysql_query("UPDATE animals SET name = '$name' WHERE user_id = $this->id");
+        mysql_close();
     }
 }
 ?>
