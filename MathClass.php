c569f902ab8b3c4d7c72cccb2fdeb948ff67cb8d
Theino <theino8@yahoo.com>
2011-04-23 17:08:52 -0700

    added static methods gcm, lcm and lcmArr

--- MathClass.php
+++ MathClass.php
@@ -1,6 +1,25 @@
 <?php
 class MathClass
 {
-    
+    static function gcm(a, b)
+    {
+	return ( b == 0 ) ? (a):( gcm(b, a % b) );
+    }
+    static function lcm(a, b)
+    {
+        return ( a / gcm(a,b) ) * b;
+    }
+    static function lcmArr($arr)
+    {
+        if (count($arr) > 1)
+        {
+	    $arr[] = lcm( array_shift($arr) , array_shift($arr) );
+            return lcmArr( $arr );
+	}
+        else
+        {
+	    return $arr[0];
+	}
+    }
 }
 ?>
