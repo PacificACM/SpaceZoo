d11e0739121e5817f0891c0d4eb62984aa275b5e
Theino <theino8@yahoo.com>
2011-04-24 13:32:55 -0700

    removed isAdmin die and added MainMenuClass

--- shipActions.php
+++ shipActions.php
@@ -12,17 +12,7 @@
     <h1 style = "text-align: center;">Space Zoo</h1>
     <?php
         $user = new UserClass($facebook->getUser());
-        if(!$user->isAdmin())
-        {
-            die("Not Admin");
-        }
-        $menu = new MenuClass();
-        $menu->addMenuItem(new MenuItemClass('Index', 'index.php', false));
-        $menu->addMenuItem(new MenuItemClass('My Home', 'myHome.php', false));
-        $menu->addMenuItem(new MenuItemClass('Current Planet', 'currentPlanet.php', false));
-        $menu->addMenuItem(new MenuItemClass('Ship Actions', 'shipActions.php', true));
-        $menu->addMenuItem(new MenuItemClass('Admin Page', 'adminPage.php', false));
-        $menu->printMenu();
+        MainMenuClass::show($user->isAdmin());
     ?>
   </body>
 </html>
