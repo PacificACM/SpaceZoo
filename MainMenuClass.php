c06534d54c5c84dd2eb90dd29393baba2bb74fcb
Theino <theino8@yahoo.com>
2011-04-24 13:28:29 -0700

    added static show method and private static addMenuItemAuto method

--- MainMenuClass.php
+++ MainMenuClass.php
@@ -1,6 +1,27 @@
 <?php
 class MainMenuClass
 {
-    
+    private static $menu;
+    private static function addMenuItemAuto($name, $file)
+    {
+        if($file == basename($_SERVER['REQUEST_URI'], ".php") . ".php")
+        {
+            $menu->addMenuItem(new MenuItemClass($name, $file, true));
+        }
+        else
+        {
+            $menu->addMenuItem(new MenuItemClass($name, $file, false));
+        }
+    }
+    static function show()
+    {
+        MainMenuClass::$menu = new MenuClass();
+        MainMenuClass::addMenuItemAuto('Index', 'index.php');
+        MainMenuClass::addMenuItemAuto('My Home', 'myHome.php');
+        MainMenuClass::addMenuItemAuto('Current Planet', 'currentPlanet.php');
+        MainMenuClass::addMenuItemAuto('Ship Actions', 'shipActions.php');
+        MainMenuClass::addMenuItemAuto('Admin Page', 'adminPage.php');
+        $menu->printMenu();
+    }
 }
 ?>
