eafc2a50a4dc0d60bdaf913a51a90a685e482f05
Theino <theino8@yahoo.com>
2011-04-23 17:01:08 -0700

    added private gcm, lcm and lcmArr methods. As well as incomplete stuff to getAnimal method.

--- PlanetClass.php
+++ PlanetClass.php
@@ -63,5 +63,51 @@ class PlanetClass
         }
         return $animalTypes;
     }
+    function gcm(a, b)
+    {
+	return ( b == 0 ) ? (a):( gcm(b, a % b) );
+    }
+    function lcm(a, b)
+    {
+        return ( a / gcm(a,b) ) * b;
+    }
+    private function lcmArr($arr)
+    {
+        if (count($arr) > 1)
+        {
+	    $arr[] = lcm( array_shift($arr) , array_shift($arr) );
+            return lcmArr( $arr );
+	}
+        else
+        {
+	    return $arr[0];
+	}
+    }
+    function pickAnimal()
+    {
+        $db = new DatabaseClass();
+        $animalTypes = $this->getAnimalTypes();
+        $numAnimalTypes = count($animalTypes);
+        //Will have to do some sort of normilization
+        //lower rarity values mean better chance that it
+        //should hit the random numberl
+        
+        $rarityArr = array();
+        foreach ($animalTypes as $currAnimal)
+        {
+            $rarityArr[] = $currAnimal->getRarity();
+        }
+        $lcmRarity = lcm_arr($rarityArr);
+        for($i = 0; $i < count($rarityArr); $i++)
+        {
+            $rarityArr[$i] = $lcmRarity/$rarityArr[$i];
+        }
+        $rarityTotal = 0;
+        for($i = 0; $i < count($rarityArr); $i++)
+        {
+            $rarityTotal += $rarityArr[$i];
+        }
+        
+    }
 }
 ?>
