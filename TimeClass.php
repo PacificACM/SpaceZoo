e639cb9b22ed67f49978f7844fabbf59e82b027b
Theino <theino8@yahoo.com>
2011-05-22 17:55:43 -0700

    added getCurrTimeAsBigInt static method

--- TimeClass.php
+++ TimeClass.php
@@ -1,6 +1,10 @@
 <?php
 class TimeClass
 {
-    
+    static function getCurrTimeAsBigInt()
+    {
+        $timeOfDayArr = gettimeofday();
+        return $timeOfDayArr['sec']*1000000 + $timeOfDayArr['usec']
+    }
 }
 ?>
